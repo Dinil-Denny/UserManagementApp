3893895c02b3fa0a8ca9abd3ca04f818a0e5623f
Dinil_Denny <dinildenny99@gmail.com>
2026-03-13 15:34:25 +0530

    Server conncected and started at port 3000

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,5 +15,7 @@ app.use(
     allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,10 @@
 //code for starting the server and connecting to the port
+
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port : number | string = process.env.PORT || 5000;
+
+app.listen(port,()=>console.log(`server started at port ${port}`));
