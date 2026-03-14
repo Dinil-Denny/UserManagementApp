95f40c476791c98a4e064d7ae1c09e857fe56bfa
Dinil_Denny <dinildenny99@gmail.com>
2026-03-14 17:37:34 +0530

    done user shcema & user entity

--- server/src/models/user/userSchema.ts
+++ server/src/models/user/userSchema.ts
@@ -1,3 +1,33 @@
 import mongoose from "mongoose";
+import { IUser } from "entities/UserEntity";
 
-const userSchema = new mongoose.Schema()
+const userSchema = new mongoose.Schema<IUser>({
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    profileImgURL : {
+        type : String
+    },
+    role : {
+        type : String,
+        enum : ['user','admin'],
+        default : 'user'
+    },
+    isBlocked : {
+        type : Boolean,
+        default : false
+    },
+},{timestamps: true}
+);
+
+export const Users = mongoose.model<IUser>('Users',userSchema);
