ded8091d42225fcc6f95db199dd58a125c8cecf9
Dinil_Denny <dinildenny99@gmail.com>
2026-03-30 11:33:22 +0530

    wrote repository interfaces and service interfaces

---
+++ server/src/interfaces/repository-interfaces/IUserRepository.ts
@@ -0,0 +1,9 @@
+import { UserEntity } from "entities/UserEntity";
+
+export interface IUserRepository{
+    createUser(user:UserEntity) : Promise<UserEntity>;
+    findByEmail(email : string) : Promise<UserEntity | null>;
+    findById(id:string) : Promise<UserEntity | null>;
+    deleteUser(id:string) : Promise<boolean>;
+    editDetails(id:string) : Promise<UserEntity>;
+}
---
+++ server/src/interfaces/service-interfaces/IUserService.ts
@@ -0,0 +1,7 @@
+import { LoginUserDTO, RegisterUserDTO } from "../../dtos/UserDTO";
+import { UserEntity } from "entities/UserEntity";
+
+export interface IUserService{
+    userLogin(data:LoginUserDTO) : Promise<any>;
+    registerUser(data:RegisterUserDTO) : Promise<UserEntity | null>;
+}
