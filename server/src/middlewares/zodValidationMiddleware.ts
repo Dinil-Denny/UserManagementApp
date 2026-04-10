696b57643799a2f6aeafb1f5e286b920b9bf89df
Dinil_Denny <dinildenny99@gmail.com>
2026-04-10 13:24:43 +0530

    Wrote Zod validation middleware

---
+++ server/src/middlewares/zodValidationMiddleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodType, ZodError } from "zod";
+
+export const inputValidator = <T>(schema : ZodType<T>) => {
+    (req:Request,res:Response,next:NextFunction) => {
+        try {
+            req.body = schema.parse(req.body);
+            next();
+        } catch (error) {
+            if(error instanceof ZodError){
+                return res.status(400).json({
+                    success : false,
+                    message : "Validation failed!",
+                    errors : error.issues.map(e => ({
+                        field : e.path.join('.'),
+                        message : e.message
+                    }))
+                });
+            };
+            //forward errors other than zoderror to error middleware
+            next(error);
+        };
+    };
+};
